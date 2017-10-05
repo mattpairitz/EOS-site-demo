91654500a540681e72ac06b26a7197247daa09b6
Matthew Pairitz <pairitma@eos31.cis.gvsu.edu>
2017-10-05 17:51:31 -0400

    First commit

---
+++ js/blink.js
@@ -0,0 +1,5 @@
+var blink_speed = 1000; // every 1000 == 1 second, adjust to suit
+var t = setInterval(function () {
+    var ele = document.getElementById('myBlinkingDiv');
+    ele.style.visibility = (ele.style.visibility == 'hidden' ? '' : 'hidden');
+}, blink_speed);
